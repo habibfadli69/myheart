ecdab5b570053f9fc4218df04194e0340a218405
habibfadli69 <habibfadli69@gmail.com>
2019-07-01 04:38:37 +0700

    c

--- sensor_server/controllers/dokter.controller.js
+++ sensor_server/controllers/dokter.controller.js
@@ -16,20 +16,20 @@ module.exports = {
     },
     updateDokter : async(req,res) => {
         let response = new Response()
-        // res.json(req)
-        // let data=await dokterRepositories.updatePasien(req.params.id, req.body.namaPasien, req.body.nicknamePasien, req.body.birthdayPasien, req.body.umurPasien, req.body.alamatPasien, req.body.tinggiPasien, req.body.beratPasien, req.body.notelpPasien, req.body.kondisi)
-        // res.json(data)
-        try {
-            response.setData(await dokterRepositories.updateDokter(req.params.id, req.body.namaDokter, req.body.nicknameDokter, req.body.birthdayDokter, req.body.umurDokter, req.body.alamatDokter, req.body.tinggiDokter, req.body.beratDokter, req.body.notelpDokter))
-        } catch (e) {
-            response.setStatus(false)
-            response.setMessage(e)
-        }
-        res.json(response) 
+        res.json(req)
+        let data=await dokterRepositories.updatePasien(req.params.id, req.body.namaPasien, req.body.nicknamePasien, req.body.birthdayPasien, req.body.umurPasien, req.body.alamatPasien, req.body.tinggiPasien, req.body.beratPasien, req.body.notelpPasien)
+        res.json(data)
+        // try {
+        //     response.setData(await dokterRepositories.updateDokter(req.params.id, req.body.namaDokter, req.body.nicknameDokter, req.body.birthdayDokter, req.body.umurDokter, req.body.alamatDokter, req.body.tinggiDokter, req.body.beratDokter, req.body.notelpDokter))
+        // } catch (e) {
+        //     response.setStatus(false)
+        //     response.setMessage(e)
+        // }
+        // res.json(response) 
     },
     updatePasien : async(req,res) => {
-        let response = new Response()
-        res.json(req)
+        // let response = new Response()
+        // res.json(req)
         // let data=await dokterRepositories.updatePasien(req.params.id, req.body.namaPasien, req.body.nicknamePasien, req.body.birthdayPasien, req.body.umurPasien, req.body.alamatPasien, req.body.tinggiPasien, req.body.beratPasien, req.body.notelpPasien, req.body.kondisi)
         // res.json(data)
         // try {
