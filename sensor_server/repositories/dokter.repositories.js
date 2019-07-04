0ad2bdc9da940770654cfcbc97b00def6ba64d34
habibfadli69 <habibfadli69@gmail.com>
2019-07-05 01:36:43 +0700

    c

--- sensor_server/repositories/dokter.repositories.js
+++ sensor_server/repositories/dokter.repositories.js
@@ -153,7 +153,7 @@ const dokterRepositories = {
         },
         {
         $push:{
-            "dokter.0.pasien.$.data":{
+            "dokter.0.pasien.$[].data":{
                 tanggal : new Date(),
                 temperature : Number(temperature),
                 heartrate: Number(heartrate),
