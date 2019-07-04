facfd867a739e8e62180a13aa985351557df14c7
habibfadli69 <habibfadli69@gmail.com>
2019-07-04 23:57:13 +0700

    C

--- sensor_server/repositories/dokter.repositories.js
+++ sensor_server/repositories/dokter.repositories.js
@@ -31,25 +31,25 @@ const dokterRepositories = {
         data : [subData]
       }
       let subDokter = {
-        namaDokter : namaDokter,
-        emailDokter : emailDokter,
-        passwordDokter : passwordDokter,
-        birthdayDokter : birthdayDokter,
-        umurDokter : umurDokter,
-        alamatDokter : alamatDokter,
-        tinggiDokter : tinggiDokter,
-        beratDokter : beratDokter,
-        notelpDokter : notelpDokter,
+        // namaDokter : namaDokter,
+        // emailDokter : emailDokter,
+        // passwordDokter : passwordDokter,
+        // birthdayDokter : birthdayDokter,
+        // umurDokter : umurDokter,
+        // alamatDokter : alamatDokter,
+        // tinggiDokter : tinggiDokter,
+        // beratDokter : beratDokter,
+        // notelpDokter : notelpDokter,
         
-        // namaDokter : null,
-        // emailDokter : null,
-        // passwordDokter : null,
-        // birthdayDokter : null,
-        // umurDokter : null,
-        // alamatDokter : null,
-        // tinggiDokter : null,
-        // beratDokter : null,
-        // notelpDokter : null,
+        namaDokter : null,
+        emailDokter : null,
+        passwordDokter : null,
+        birthdayDokter : null,
+        umurDokter : null,
+        alamatDokter : null,
+        tinggiDokter : null,
+        beratDokter : null,
+        notelpDokter : null,
 
         pasien : [subPasien]
       }
