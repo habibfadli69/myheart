f51c064ac1f85b0c999c92b0139d1aba9041ed81
habibfadli69 <habibfadli69@gmail.com>
2019-07-16 09:23:32 +0700

    C

--- sensor_server/controllers/dokter.controller.js
+++ sensor_server/controllers/dokter.controller.js
@@ -51,19 +51,19 @@ module.exports = {
             "berat" : req.body.beratPasien, 
             "notelp" : req.body.notelpPasien
         }) 
-        try {
-            // response.setData(await dokterRepositories.updatePasien(req.params.id, req.body.namaPasien, req.body.emailPasien, req.body.passwordPasien, req.body.birthdayPasien, req.body.umurPasien, req.body.alamatPasien, req.body.tinggiPasien, req.body.beratPasien, req.body.notelpPasien))
-            let id = req.params.id
-            await dokterRepositories.updatePasien(id, req.body.namaPasien, req.body.emailPasien, req.body.birthdayPasien, req.body.umurPasien, req.body.alamatPasien, req.body.tinggiPasien, req.body.beratPasien, req.body.notelpPasien)
+        // try {
+        //     // response.setData(await dokterRepositories.updatePasien(req.params.id, req.body.namaPasien, req.body.emailPasien, req.body.passwordPasien, req.body.birthdayPasien, req.body.umurPasien, req.body.alamatPasien, req.body.tinggiPasien, req.body.beratPasien, req.body.notelpPasien))
+        //     let id = req.params.id
+        //     await dokterRepositories.updatePasien(id, req.body.namaPasien, req.body.emailPasien, req.body.birthdayPasien, req.body.umurPasien, req.body.alamatPasien, req.body.tinggiPasien, req.body.beratPasien, req.body.notelpPasien)
             
-            response = {
-                setStatus: 200,
-                setMessage: 'success'
-            }
-        } catch (e) {
-            response.setStatus(false)
-            response.setMessage(e)
-        }
+        //     response = {
+        //         setStatus: 200,
+        //         setMessage: 'success'
+        //     }
+        // } catch (e) {
+        //     response.setStatus(false)
+        //     response.setMessage(e)
+        // }
         // res.json(response) 
     },
 
