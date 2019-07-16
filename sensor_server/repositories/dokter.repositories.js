0214dd323bd36287d010ecc1e48d83a8df66dcf6
habibfadli69 <habibfadli69@gmail.com>
2019-07-16 21:13:36 +0700

    c

--- sensor_server/repositories/dokter.repositories.js
+++ sensor_server/repositories/dokter.repositories.js
@@ -148,16 +148,7 @@ const dokterRepositories = {
   },
 
   updatePasien: async (id, namaPasien, emailPasien, passwordPasien, birthdayPasien, umurPasien, alamatPasien, tinggiPasien, beratPasien, notelpPasien, kondisi) => {
-    let updateDokter = await Dokter.findByIdAndUpdate("5d1e33b1087fc600183f1f1b",{
-      namaDokter : "Haryono"
-      // if (!err) {
-      //   await dokter.pasien.findByIdAndUpdate("5d2ca41250426f0018e121bb", {
-      //     namaPasien: "Imam"
-      //   });
-      // } else {
-      //   console.log(err);
-      // }
-    });
+    let updateDokter = await Dokter.findById("5d1e33b1087fc600183f1f1b");
     return updateDokter;
     // let pasienUpdate = await Dokter.update({
     // "dokter._id" : "5d1e33b1087fc600183f1f1b",
