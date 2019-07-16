5fbd8e92b036e1fd410a9e834736942c4e648f12
habibfadli69 <habibfadli69@gmail.com>
2019-07-16 20:34:00 +0700

    c

--- sensor_server/repositories/dokter.repositories.js
+++ sensor_server/repositories/dokter.repositories.js
@@ -149,15 +149,14 @@ const dokterRepositories = {
 
     updatePasien : async(id, namaPasien, emailPasien, passwordPasien, birthdayPasien, umurPasien, alamatPasien, tinggiPasien, beratPasien, notelpPasien, kondisi) => {
       let pasienUpdate = await Dokter.update({
-      "_id" : "5d1e33b1087fc600183f1f1a",
-      "dokter._id" : "5d1e33b1087fc600183f1f1b",
+        
       "dokter.pasien._id" : id,
         // _id : id, 
       },
       {
         $set : {
           // dokter.$[].pasien.$[].
-          "dokter.0.pasien.$[].namaPasien" : namaPasien,
+          "dokter.0.pasien.[].namaPasien" : namaPasien,
           // "dokter.$[].pasien.$[].emailPasien" : emailPasien,
           // "dokter.$[].pasien.$[].passwordPasien" : passwordPasien,
           // "dokter.$[].pasien.$[].birthdayPasien" : birthdayPasien,
